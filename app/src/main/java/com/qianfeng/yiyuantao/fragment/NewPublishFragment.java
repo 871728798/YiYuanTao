0ec455faa22bc95054c625ade222c8cdefaf51cf
lirenhua1 <18378405409m0.@sina.cn>
2015-11-23 09:08:03 +0800

    最新揭晓  下拉刷新图标

--- app/src/main/java/com/qianfeng/yiyuantao/fragment/NewPublishFragment.java
+++ app/src/main/java/com/qianfeng/yiyuantao/fragment/NewPublishFragment.java
@@ -6,13 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 import com.qianfeng.yiyuantao.R;
 
 /**
  * Created by Administrator on 2015/11/21 0021.
  */
 public class NewPublishFragment extends Fragment{
-
+    private PullToRefreshListView pullToRefresh;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_publish, container, false);

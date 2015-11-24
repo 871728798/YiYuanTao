6a29a00a00f4c025383f3dfd2e3a64ebc30927c8
lxl <asdzasdz3311058@qq.com>
2015-11-24 17:59:41 +0800

    第二个页面初步效果2

---
+++ app/src/main/java/com/qianfeng/yiyuantao/adapter/AllCommPlAdapter.java
@@ -0,0 +1,38 @@
+package com.qianfeng.yiyuantao.adapter;
+
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.qianfeng.yiyuantao.R;
+import com.qianfeng.yiyuantao.bean.PrizeEntity;
+import com.qianfeng.yiyuantao.util.NetUtils;
+
+/**
+ * Created by LXL on 2015/11/24.
+ */
+public class AllCommPlAdapter extends ABSBaseAdapter<PrizeEntity> {
+
+    public AllCommPlAdapter(Context context) {
+        super(context, R.layout.item_lv_allcom);
+    }
+
+    @Override
+    public void bindDatas(ViewHolder viewHolder, PrizeEntity data, int position) {
+        ImageView iv_thumb_nail_allomm = (ImageView) viewHolder.getView(R.id.iv_thumb_nail_allomm);
+        TextView tv_peroid_allcomm = (TextView) viewHolder.getView(R.id.tv_peroid_allcomm);
+        TextView tv_title_comm = (TextView) viewHolder.getView(R.id.tv_title_comm);
+        TextView tv_price_allcomm = (TextView) viewHolder.getView(R.id.tv_price_allcomm);
+        ProgressBar progressBar_comm = (ProgressBar) viewHolder.getView(R.id.progressBar_comm);
+        TextView tv_count_else_allcomm = (TextView) viewHolder.getView(R.id.tv_count_else_allcomm);
+
+        NetUtils.disImageView(data.getThumb_nail(),iv_thumb_nail_allomm,R.mipmap.all_loading_1,R.mipmap.all_chacha);
+        tv_peroid_allcomm.setText("第" + data.getPeroid() + "期  ");
+        tv_title_comm.setText(data.getTitle());
+        tv_price_allcomm.setText("总需" + data.getPrice());
+        tv_count_else_allcomm.setText("剩余" + data.getCount_else());
+        progressBar_comm.setMax(data.getPrice());
+        progressBar_comm.setSecondaryProgress(data.getCount_else());
+    }
+}
